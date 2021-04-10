9b6437be133ad9f8da1a5b76936e6b0196d0d027
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-10 12:27:44 +0100

    ✨ Add return for error messages

--- index.ts
+++ index.ts
@@ -23,9 +23,9 @@ app.get("/dot/releases/:os/:arch/:type", (req, res) => {
     const dot = products.find((p: any) => p.name == "dot");
 
     if(dot) {
-        if(!dot.os.includes(req.params.os)) res.status(400).end(`Specified OS is invalid. Valid options: ${JSON.stringify(dot.os)}`)
-        if(!dot.arch.includes(req.params.arch)) res.status(400).end(`Specified architecture is invalid. Valid options: ${JSON.stringify(dot.arch)}`)
-        if(!dot.types.includes(req.params.type)) res.status(400).end(`Specified type is invalid. Valid options: ${JSON.stringify(dot.types)}`)
+        if(!dot.os.includes(req.params.os)) return res.status(400).end(`Specified OS is invalid. Valid options: ${JSON.stringify(dot.os)}`)
+        if(!dot.arch.includes(req.params.arch)) return res.status(400).end(`Specified architecture is invalid. Valid options: ${JSON.stringify(dot.arch)}`)
+        if(!dot.types.includes(req.params.type)) return res.status(400).end(`Specified type is invalid. Valid options: ${JSON.stringify(dot.types)}`)
     
         axios.get("https://api.github.com/repos/dothq/browser-desktop/releases")
             .then(r => {
@@ -36,12 +36,12 @@ app.get("/dot/releases/:os/:arch/:type", (req, res) => {
                         if(req.params.os == "linux" && asset.name.startsWith("dot-") && asset.name.endsWith(".tar.bz2") && req.params.type == "raw") return asset
                     });
     
-                    if(found && found.browser_download_url) res.redirect(found.browser_download_url)
-                    else res.status(404).end("No releases found.")
+                    if(found && found.browser_download_url) return res.redirect(found.browser_download_url)
+                    else return res.status(404).end("No releases found.")
                 })
             })
     } else {
-        res.status(500);
+        return res.status(500);
     }
 });
 
