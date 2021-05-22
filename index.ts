4169c6901645ed37e39ee8d70537306bafebedd9
trickypr <trickypr@icloud.com>
2021-05-22 16:12:44 +1000

    🐛 Use latest, not oldest, release

--- index.ts
+++ index.ts
@@ -1,55 +1,86 @@
-import express from 'express';
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
 
 const products = [
     {
         name: "dot",
         arch: ["x86"],
-        os: [
-            "windows",
-            "linux",
-            "macos"
-        ],
-        types: [
-            "raw",
-            "installer"
-        ]
+        os: ["windows", "linux", "macos"],
+        types: ["raw", "installer"]
     }
-]
+];
 
 const app = express();
 
 process.on("uncaughtException", (e) => {
-    console.log(e.message, e.stack)
-})
+    console.log(e.message, e.stack);
+});
 
 app.get("/dot/releases/:os/:arch/:type", async (req, res) => {
     const dot = products.find((p: any) => p.name == "dot");
 
-    if(dot) {
-        if(!dot.os.includes(req.params.os)) return res.status(400).send(`Specified OS is invalid. Valid options: ${JSON.stringify(dot.os)}`)
-        if(!dot.arch.includes(req.params.arch)) return res.status(400).send(`Specified architecture is invalid. Valid options: ${JSON.stringify(dot.arch)}`)
-        if(!dot.types.includes(req.params.type)) return res.status(400).send(`Specified type is invalid. Valid options: ${JSON.stringify(dot.types)}`)
-    
+    if (dot) {
+        if (!dot.os.includes(req.params.os))
+            return res
+                .status(400)
+                .send(
+                    `Specified OS is invalid. Valid options: ${JSON.stringify(
+                        dot.os
+                    )}`
+                );
+        if (!dot.arch.includes(req.params.arch))
+            return res
+                .status(400)
+                .send(
+                    `Specified architecture is invalid. Valid options: ${JSON.stringify(
+                        dot.arch
+                    )}`
+                );
+        if (!dot.types.includes(req.params.type))
+            return res
+                .status(400)
+                .send(
+                    `Specified type is invalid. Valid options: ${JSON.stringify(
+                        dot.types
+                    )}`
+                );
+
         let found: any;
 
-        const r = await axios.get("https://api.github.com/repos/dothq/browser-desktop/releases")
+        const r = await axios.get(
+            "https://api.github.com/repos/dothq/browser-desktop/releases"
+        );
 
-        r.data.forEach((release: any) => {
+        r.data.reverse().forEach((release: any) => {
             const f = release.assets.find((asset: any) => {
-                if(req.params.os == "windows" && asset.name.startsWith("Install") && asset.name.endsWith(".exe")) return asset
-                if(req.params.os == "macos" && asset.name.startsWith("Dot") && asset.name.endsWith(".dmg")) return asset
-                if(req.params.os == "linux" && asset.name.startsWith("dot-") && asset.name.endsWith(".tar.bz2")) return asset
+                if (
+                    req.params.os == "windows" &&
+                    asset.name.startsWith("Install") &&
+                    asset.name.endsWith(".exe")
+                )
+                    return asset;
+                if (
+                    req.params.os == "macos" &&
+                    asset.name.startsWith("Dot") &&
+                    asset.name.endsWith(".dmg")
+                )
+                    return asset;
+                if (
+                    req.params.os == "linux" &&
+                    asset.name.startsWith("dot-") &&
+                    asset.name.endsWith(".tar.bz2")
+                )
+                    return asset;
             });
 
-            if(f) found = f;
-        })
+            if (f) found = f;
+        });
 
-        console.log(found)
+        console.log(found);
 
-        if(found) res.redirect(found.browser_download_url)
+        if (found) res.redirect(found.browser_download_url);
         else {
-            res.status(404).send("No releases found.")
+            res.status(404).send("No releases found.");
             return;
         }
     } else {
@@ -57,6 +88,6 @@ app.get("/dot/releases/:os/:arch/:type", async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Download server started at ${port}`))
+app.listen(port, () => console.log(`Download server started at ${port}`));
